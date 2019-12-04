37eabe3b0e1f3e3e670513c6d5e8fe973e9cb40c
DaneWoods <megadster@comcast.net>
2019-12-04 13:51:54 -0800

    Completed Lab6

--- CustomerProductDB/ProductPropsTests/ProductTests.cs
+++ CustomerProductDB/ProductPropsTests/ProductTests.cs
@@ -80,103 +80,96 @@ namespace ProductPropsTests
         [Test]
         public void TestUpdate()
         {
-            Customer c = new Customer(1, dataSource);
-            c.Name = "Mario, Mario";
-            c.Address = "Bowser's Kingdom";
-            c.Save();
+            Product p = new Product(1, dataSource);
+            p.ProductCode = "GJ5K";
+            p.Description = "The Infinity Stones";
+            p.Save();
 
-            c = new Customer(1, dataSource);
-            Assert.AreEqual(c.ID, 1);
-            Assert.AreEqual(c.Name, "Mario, Mario");
-            Assert.AreEqual(c.Address, "Bowser's Kingdom");
+            p = new Product(1, dataSource);
+            Assert.AreEqual(p.ProductID, 1);
+            Assert.AreEqual(p.ProductCode.Trim(), "GJ5K");
+            Assert.AreEqual(p.Description, "The Infinity Stones");
         }
 
         [Test]
         public void TestDelete()
         {
-            Customer c = new Customer(2, dataSource);
-            c.Delete();
-            c.Save();
-            Assert.Throws<Exception>(() => new Customer(2, dataSource));
+            Product p = new Product(2, dataSource);
+            p.Delete();
+            p.Save();
+            Assert.Throws<Exception>(() => new Product(2, dataSource));
         }
 
         // *** I added this
         [Test]
         public void TestGetList()
         {
-            Customer c = new Customer(dataSource);
-            List<Customer> customers = (List<Customer>)c.GetList();
-            Assert.AreEqual(696, customers.Count);
-            Assert.AreEqual(1, customers[0].ID);
-            Assert.AreEqual("1108 Johanna Bay Drive", customers[0].Address);
+            Product p = new Product(dataSource);
+            List<Product> products = (List<Product>)p.GetList();
+            Assert.AreEqual(16, products.Count);
+            Assert.AreEqual(1, products[0].ProductID);
+            Assert.AreEqual("Murach's ASP.NET 4 Web Programming with C# 2010", products[0].Description);
         }
 
         [Test]
         public void TestNoRequiredPropertiesNotSet()
         {
             // not in Data Store - userid, title and description must be provided
-            Customer c = new Customer(dataSource);
-            Assert.Throws<Exception>(() => c.Save());
+            Product p = new Product(dataSource);
+            Assert.Throws<Exception>(() => p.Save());
         }
 
         [Test]
         public void TestSomeRequiredPropertiesNotSet()
         {
             // not in Data Store - userid, title and description must be provided
-            Customer c = new Customer(dataSource);
-            Assert.Throws<Exception>(() => c.Save());
-            c.Name = "Mario, Mario";
-            Assert.Throws<Exception>(() => c.Save());
-            c.Address = "Mushroom Kingdom";
-            Assert.Throws<Exception>(() => c.Save());
+            Product p = new Product(dataSource);
+            Assert.Throws<Exception>(() => p.Save());
+            p.ProductCode = "GK38";
+            Assert.Throws<Exception>(() => p.Save());
+            p.Description = "Piece Coal";
+            Assert.Throws<Exception>(() => p.Save());
         }
 
         [Test]
         public void TestInvalidPropertyNameSet()
         {
-            Customer c = new Customer(dataSource);
-            Assert.Throws<ArgumentOutOfRangeException>(() => c.Name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Product p = new Product(dataSource);
+            Assert.Throws<ArgumentOutOfRangeException>(() => p.ProductCode = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
 
         [Test]
         public void TestInvalidPropertyAddressSet()
         {
-            Customer c = new Customer(dataSource);
-            Assert.Throws<ArgumentException>(() => c.Address = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Product p = new Product(dataSource);
+            Assert.Throws<ArgumentException>(() => p.Description = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
 
         [Test]
         public void TestInvalidPropertyCitySet()
         {
-            Customer c = new Customer(dataSource);
-            Assert.Throws<ArgumentException>(() => c.City = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Product p = new Product(dataSource);
+            Assert.Throws<ArgumentException>(() => p.UnitPrice = -1);
         }
 
         [Test]
         public void TestInvalidPropertyStateSet()
         {
-            Customer c = new Customer(dataSource);
-            Assert.Throws<ArgumentException>(() => c.State = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        }
-
-        [Test]
-        public void TestInvalidPropertyZipcodeSet()
-        {
-            Customer c = new Customer(dataSource);
-            Assert.Throws<ArgumentException>(() => c.ZipCode = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Product p = new Product(dataSource);
+            Assert.Throws<ArgumentException>(() => p.OnHandQuantity = -1);
         }
 
         // *** I added this
         [Test]
         public void TestConcurrencyIssue()
         {
-            Customer e1 = new Customer(1, dataSource);
-            Customer e2 = new Customer(1, dataSource);
+            Product e1 = new Product(1, dataSource);
+            Product e2 = new Product(1, dataSource);
 
-            e1.Address = "Mushroom Kingdom";
+            e1.UnitPrice = 20;
             e1.Save();
 
-            e2.Address = "Bowser's Kingdom";
+            e2.UnitPrice = 19;
             Assert.Throws<Exception>(() => e2.Save());
         }
     }
